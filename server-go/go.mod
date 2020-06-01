a231fc0059ef98cce0d4a99b3b34cae09ec8c91e
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-01 15:09:47 +0200

    Refactored code

--- server-go/go.mod
+++ server-go/go.mod
@@ -3,11 +3,14 @@ module zsbrybnik.pl/server-go
 go 1.14
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gbrlsnchs/jwt/v3 v3.0.0-rc.2 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
 	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
--- server-go/main.go
+++ server-go/main.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type verifyTokenJSON struct {
+	Token string `json:"token" binding:"required"`
+}
+
+type loginJSON struct {
+	Login    string `json:"login" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 func errorHandler(err error, critical bool) {
 	if err != nil {
@@ -28,10 +38,52 @@ func createBCryptHash(password string) string {
 	return hashedPassword
 }
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if context.Request.Method == "OPTIONS" {
+			context.Abort()
+			return
+		}
+		context.Next()
+	}
+}
+
+func setDatabase(database *sql.DB) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		context.Keys["database"] = database
+		context.Next()
+	}
+}
+
+var secretKey = jwt.NewHS256([]byte("secret"))
+
+/*func verifyToken(context *gin.Context, token string) error {
+  _, err := jwt.Verify(token, secretKey,  )
+}*/
+
+func verifyTokenHandler(context *gin.Context) {
+	var verifyTokenData verifyTokenJSON
+	err := context.Bind(&verifyTokenData)
+	errorHandler(err, false)
+	context.JSON(200, verifyTokenData)
+}
+
+func loginHandler(context *gin.Context) {
+	var loginData loginJSON
+	err := context.Bind(&loginData)
+	errorHandler(err, false)
+}
+
 func main() {
-	database, err := sql.Open("mysql", "root:localhost:3306/zsbrybnik")
+	database, err := sql.Open("mysql", "root:localhost@/zsbrybnik")
 	errorHandler(err, true)
 	defer database.Close()
 	server := gin.Default()
+	server.Use(corsMiddleware())
+	server.Use(setDatabase(database))
+	server.POST("/api/verify-token", verifyTokenHandler)
+	server.POST("/api/login", loginHandler)
 	server.Run(":5002")
 }
