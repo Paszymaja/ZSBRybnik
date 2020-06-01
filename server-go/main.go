2ad9c40e7159193208652bb149d3a9b83f229249
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-01 13:29:17 +0200

    Refactoring code
    Co-authored-by: borys-malinowski <bmalinowski.cisco2017@gmail.com>

--- server-go/main.go
+++ server-go/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
+func errorHandler(err error, critical bool) {
+	if err != nil {
+		if critical {
+			log.Fatalln(err)
+		} else {
+			fmt.Println(err)
+		}
+	}
+}
+
+func createBCryptHash(password string) string {
+	passwordInBytes := []byte(password)
+	hashedPasswordInBytes, err := bcrypt.GenerateFromPassword(passwordInBytes, 15)
+	errorHandler(err, false)
+	hashedPassword := string(hashedPasswordInBytes)
+	return hashedPassword
+}
+
 func main() {
-	
+	database, err := sql.Open("mysql", "root:localhost:3306/zsbrybnik")
+	errorHandler(err, true)
+	defer database.Close()
+	server := gin.Default()
+	server.Run(":5002")
 }
