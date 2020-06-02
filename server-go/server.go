82cdbcf03458b3b861030d68266005007ee613f4
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-02 13:24:36 +0200

    Refactored old code
    Co-authored-by: borys-malinowski <bmalinowski.cisco2017@gmail.com>

--- server-go/server.go
+++ server-go/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/gin-gonic/gin"
@@ -20,8 +22,27 @@ type loginJSON struct {
 	Password string `json:"password" binding:"required"`
 }
 
+type loginMySQLResult struct {
+	CorrectLoginPassword bool   `json:"correctLoginPassword"`
+	Password             string `json:"password"`
+}
+
+type postJSON struct {
+	Id           int    `json:"id"`
+	Title        string `json:"title"`
+	Img          string `json:"img"`
+	Introduction string `json:"introduction"`
+	ImgAlt       string `json:"imgAlt"`
+}
+
 type customJWTPayload struct {
 	jwt.Payload
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type changePasswordJSON struct {
+	Token    string `json:"token"`
 	Login    string `json:"login"`
 	Password string `json:"password"`
 }
@@ -65,9 +86,12 @@ func setDatabase(database *sql.DB) gin.HandlerFunc {
 
 var secretKey = jwt.NewHS256([]byte("secret"))
 
-/*func verifyToken(context *gin.Context, token string) error {
-  _, err := jwt.Verify(token, secretKey,  )
-}*/
+func verifyToken(context *gin.Context, token string) error {
+	var tokenStructure customJWTPayload
+	tokenInBytes := []byte(token)
+	_, err := jwt.Verify(tokenInBytes, secretKey, &tokenStructure)
+	return err
+}
 
 func verifyTokenHandler(context *gin.Context) {
 	var verifyTokenData verifyTokenJSON
@@ -80,38 +104,121 @@ func loginHandler(context *gin.Context) {
 	var loginData loginJSON
 	err := context.Bind(&loginData)
 	errorHandler(err, false)
-	query := "SELECT EXISTS(SELECT * FROM admins WHERE login = ?) AS correctLoginAndPassword, password FROM admins WHERE login = ?"
-	database, ok := context.MustGet("database").(*sql.DB)
-	if !ok {
-		log.Fatalln("Can't find database in gin-gonic context")
-	}
-	result := database.QueryRow(query, loginData.Login, loginData.Login)
-	var loginMySQLResult loginMySQLResult
-	err = result.Scan(&loginMySQLResult.CorrectLoginPassword, &loginMySQLResult.Password)
-	errorHandler(err, false)
-	if loginMySQLResult.CorrectLoginPassword {
-		passwordInBytes := []byte(loginData.Password)
-		hashedPasswordInBytes := []byte(loginMySQLResult.Password)
-		err := bcrypt.CompareHashAndPassword(hashedPasswordInBytes, passwordInBytes)
-		errorHandler(err, false)
-		if err == nil {
-			tokenSignPayload := customJWTPayload{
-				Payload:  jwt.Payload{},
-				Login:    loginData.Login,
-				Password: loginMySQLResult.Password,
-			}
-			token, err := jwt.Sign(tokenSignPayload, secretKey)
+	if err != nil {
+		context.AbortWithError(400, errors.New("Bad Request"))
+	} else {
+		database, ok := context.MustGet("database").(*sql.DB)
+		if !ok {
+			log.Fatalln("Can't find database in gin-gonic context")
+			context.AbortWithError(500, errors.New("Internal Server Error"))
+		} else {
+			query := "SELECT EXISTS(SELECT * FROM admins WHERE login = ?) AS correctLoginAndPassword, password FROM admins WHERE login = ?"
+			result := database.QueryRow(query, loginData.Login, loginData.Login)
+			var loginMySQLResult loginMySQLResult
+			err = result.Scan(&loginMySQLResult.CorrectLoginPassword, &loginMySQLResult.Password)
 			errorHandler(err, false)
-			context.JSON(200, gin.H{
-				"token": token,
-			})
+			if err != nil {
+				context.AbortWithError(401, errors.New("Unauthorized"))
+			} else {
+				if loginMySQLResult.CorrectLoginPassword {
+					passwordInBytes := []byte(loginData.Password)
+					hashedPasswordInBytes := []byte(loginMySQLResult.Password)
+					err := bcrypt.CompareHashAndPassword(hashedPasswordInBytes, passwordInBytes)
+					errorHandler(err, false)
+					if err != nil {
+						context.AbortWithError(401, errors.New("Unauthorized"))
+					} else {
+						tokenSignPayload := customJWTPayload{
+							Payload:  jwt.Payload{},
+							Login:    loginData.Login,
+							Password: loginMySQLResult.Password,
+						}
+						token, err := jwt.Sign(tokenSignPayload, secretKey)
+						errorHandler(err, false)
+						context.JSON(200, gin.H{
+							"token": string(token),
+						})
+					}
+				}
+			}
 		}
 	}
 }
 
-type loginMySQLResult struct {
-	CorrectLoginPassword bool   `json:"correctLoginPassword"`
-	Password             string `json:"password"`
+func getPostsHandler(context *gin.Context) {
+	toSubstract := context.DefaultQuery("toSubstract", "0")
+	database, ok := context.MustGet("database").(*sql.DB)
+	if !ok {
+		log.Fatalln("Can't find database in gin-gonic context")
+		context.AbortWithError(500, errors.New("Internal Server Error"))
+	} else {
+		query := "SELECT zsbrybnik.posts.id, zsbrybnik.posts.title, zsbrybnik.posts.img, zsbrybnik.posts.introduction, zsbrybnik.posts.img_alt as imgAlt FROM zsbrybnik.posts WHERE zsbrybnik.posts.id > ((SELECT MAX(zsbrybnik.posts.id) as highestId FROM zsbrybnik.posts) - ?) AND zsbrybnik.posts.id <= ((SELECT MAX(zsbrybnik.posts.id) as highestId FROM zsbrybnik.posts) - ?) ORDER BY zsbrybnik.posts.id DESC"
+		toSubstractAsNumber, err := strconv.Atoi(toSubstract)
+		errorHandler(err, false)
+		if err != nil {
+			context.AbortWithError(400, errors.New("Bad Request"))
+		} else {
+			toSubstractBorderPost := strconv.Itoa(toSubstractAsNumber + 10)
+			result, err := database.Query(query, toSubstractBorderPost, toSubstract)
+			errorHandler(err, false)
+			defer result.Close()
+			if err != nil {
+				context.AbortWithError(500, errors.New("Internal Server Error"))
+			} else {
+				var postsArray []postJSON
+				for result.Next() {
+					var post postJSON
+					err := result.Scan(&post.Id, &post.Title, &post.Img, &post.Introduction, &post.ImgAlt)
+					errorHandler(err, false)
+					postsArray = append(postsArray, post)
+				}
+				context.JSON(200, gin.H{
+					"data": postsArray,
+				})
+			}
+		}
+	}
+}
+
+func changePasswordHandler(context *gin.Context) {
+	var changePassword changePasswordJSON
+	err := context.Bind(&changePassword)
+	errorHandler(err, false)
+	if err != nil {
+		context.AbortWithError(400, errors.New("Bad Request"))
+	} else {
+		err := verifyToken(context, changePassword.Token)
+		errorHandler(err, false)
+		if err != nil {
+			context.AbortWithError(403, errors.New("Forbidden"))
+		} else {
+			passwordInBytes := []byte(changePassword.Password)
+			hashedPasswordInBytes, err := bcrypt.GenerateFromPassword(passwordInBytes, 15)
+			errorHandler(err, false)
+			if err != nil {
+				context.AbortWithError(500, errors.New("Internal Server Error"))
+			} else {
+				hashedPassword := string(hashedPasswordInBytes)
+				database, ok := context.MustGet("database").(*sql.DB)
+				if !ok {
+					log.Fatalln("Can't find database in gin-gonic context")
+					context.AbortWithError(500, errors.New("Internal Server Error"))
+				} else {
+					query := "UPDATE zsbrybnik.admins SET zsbrybnik.admins.password = ? WHERE zsbrybnik.admins.login = ?"
+					result, err := database.Query(query, hashedPassword, changePassword.Login)
+					errorHandler(err, false)
+					defer result.Close()
+					if err != nil {
+						context.AbortWithError(500, errors.New("Internal Server Error"))
+					} else {
+						context.JSON(200, gin.H{
+							"status": "Ok!",
+						})
+					}
+				}
+			}
+		}
+	}
 }
 
 func main() {
@@ -123,5 +230,7 @@ func main() {
 	server.Use(setDatabase(database))
 	server.POST("/api/verify-token", verifyTokenHandler)
 	server.POST("/api/login", loginHandler)
+	server.GET("/api/get-posts", getPostsHandler)
+	server.POST("/api/change-password", changePasswordHandler)
 	server.Run(":5002")
 }
