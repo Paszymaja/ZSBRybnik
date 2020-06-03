0f7c3704e0a9746b52911617e7108a8ec95cbf50
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-03 21:19:15 +0200

    Refactored old code

--- website/src/components/Presentation.tsx
+++ website/src/components/Presentation.tsx
@@ -5,6 +5,7 @@ import GlobalContext from "../stores/globalStore";
 import PresentationSection from './PresentationSection';
 import PresentationImageBlock from './PresentationImageBlock';
 import PresentationContentFitImageBlock from './PresentationContentFitImageBlock';
+import PresentationImageSection from './PresentationImageSection';
 
 interface PresentationProps { }
 
@@ -59,22 +60,22 @@ const Presentation: FC<PresentationProps> = (_props: PresentationProps): JSX.Ele
           <h3>Technikum akademickie</h3>
           <PresentationSection>
             <div>Posiadamy status techników akademickich następujących ośrodków:</div>
-            <div className="presentation-section-img-block">
+            <PresentationImageSection>
               <PresentationImageBlock src="/images/silesian-technical-university.png" alt="Politechnika Śląska" loading="lazy" width="50" height="50" />
               <div>Politechnika śląska</div>
-            </div>
-            <div className="presentation-section-img-block">
-              <PresentationImageBlock className="presentation-img" src="/images/logo-cki.png" alt="Politechnika Śląska" loading="lazy" width="50" height="50" />
+            </PresentationImageSection>
+            <PresentationImageSection>
+              <PresentationImageBlock src="/images/logo-cki.png" alt="Politechnika Śląska" loading="lazy" width="50" height="50" />
               <div>Centrum Kształcenia Inżynierów</div>
-            </div>
-            <div className="presentation-section-img-block">
-              <PresentationImageBlock className="presentation-img" src="/images/logo-us.png" alt="Politechnika Śląska" loading="lazy" width="50" height="50" />
+            </PresentationImageSection>
+            <PresentationImageSection>
+              <PresentationImageBlock src="/images/logo-us.png" alt="Politechnika Śląska" loading="lazy" width="50" height="50" />
               <div>Uniwersytet śląski</div>
-            </div>
-            <div className="presentation-section-img-block">
-              <PresentationImageBlock className="presentation-img" src="/images/wst-logo.png" alt="Wyższa szkoła techniczna" loading="lazy" width="50" height="50" />
+            </PresentationImageSection>
+            <PresentationImageSection>
+              <PresentationImageBlock src="/images/wst-logo.png" alt="Wyższa szkoła techniczna" loading="lazy" width="50" height="50" />
               <div>Wyższa szkoła techniczna</div>
-            </div>
+            </PresentationImageSection>
           </PresentationSection>
         </div>
       </PresentationBlock>
@@ -83,18 +84,18 @@ const Presentation: FC<PresentationProps> = (_props: PresentationProps): JSX.Ele
           <h3>Projekty</h3>
           <PresentationSection>
             <div>Bierzemy udział w następujących projektach:</div>
-            <div className="presentation-section-img-block">
+            <PresentationImageSection>
               <PresentationImageBlock src="/images/eTwinning.png" alt="eTwinning" loading="lazy" width="50" height="50" />
               <div>eTwinning</div>
-            </div>
-            <div className="presentation-section-img-block">
+            </PresentationImageSection>
+            <PresentationImageSection>
               <PresentationImageBlock src="/images/erasmus.jpg" alt="Erasmus +" loading="lazy" width="50" height="50" />
               <div>Erasmus +</div>
-            </div>
-            <div className="presentation-section-img-block">
+            </PresentationImageSection>
+            <PresentationImageSection>
               <PresentationImageBlock src="/images/logo-zsb-project.png" alt="Konkurencyjność uczniów ZSB w Rybniku na rynku pracy" loading="lazy" width="50" height="50" />
               <div>Konkurencyjność uczniów ZSB w Rybniku na rynku pracy</div>
-            </div>
+            </PresentationImageSection>
           </PresentationSection>
         </div>
       </PresentationBlock>
@@ -103,9 +104,9 @@ const Presentation: FC<PresentationProps> = (_props: PresentationProps): JSX.Ele
           <h3>BIP</h3>
           <PresentationSection>
             <div>Biuletyn Informacji Publicznej:</div>
-            <div className="presentation-section-img-block">
+            <PresentationImageSection>
               <a href="http://zsb.bip.edukacja.rybnik.eu/" rel="noopener noreferrer" title="Biuletyn Informacji Publicznej"><PresentationImageBlock src="/images/bip.png" alt="Biuletyn Informacji Publicznej" loading="lazy" width="50" height="50" /></a>
-            </div>
+            </PresentationImageSection>
           </PresentationSection>
         </div>
       </PresentationBlock>
@@ -114,9 +115,9 @@ const Presentation: FC<PresentationProps> = (_props: PresentationProps): JSX.Ele
           <h3>Rybnicka Platofrma Poradnictwa Zawodowego</h3>
           <PresentationSection>
             <div>Rybnicka Platofrma Poradnictwa Zawodowego:</div>
-            <div className="presentation-section-img-block">
+            <PresentationImageSection>
               <a href="https://www.facebook.com/pages/category/Community/Rybnicka-Platforma-Poradnictwa-Zawodowego-187220987983602" rel="noopener noreferrer" title="Rybnicka Platofrma Poradnictwa Zawodowego"><PresentationImageBlock src="/images/rbkjobplatform.jpg" alt="Rybnicka Platofrma Poradnictwa Zawodowego" loading="lazy" width="50" height="50" /></a>
-            </div>
+            </PresentationImageSection>
           </PresentationSection>
         </div>
       </PresentationBlock>
---
+++ website/src/components/PresentationImageSection.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const PresentationImageSection = styled.div`
+  margin-top: 10px;
+`;
+
+export default PresentationImageSection;
