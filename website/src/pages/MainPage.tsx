895580ff3275eb5f6aee389a8c4da0086a9a7e59
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-04 17:45:17 +0200

    Refactored old code

--- website/src/pages/MainPage.tsx
+++ website/src/pages/MainPage.tsx
@@ -1,11 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useContext, useEffect } from 'react';
 import Page from '../components/Page';
+import { useTranslation, UseTranslationResponse } from 'react-i18next';
+import GlobalContext from '../stores/globalStore';
+import subscribeGoogleAnalytics from '../other/subscribeGoogleAnalytics';
+import { useHistory } from 'react-router-dom';
 
 interface MainPageProps { }
 
 const MainPage: FC<MainPageProps> = (): JSX.Element => {
+  const history = useHistory();
+  const { t }: UseTranslationResponse = useTranslation();
+  const { isOnlineDispatcher } = useContext(GlobalContext);
+  const [isOnline] = isOnlineDispatcher;
+  const title: string = isOnline ? t('pages.home') : "Strona główna";
+  useEffect((): void => {
+    subscribeGoogleAnalytics(history);
+  }, [history]);
   return (
-    <Page title="Strona główna">
+    <Page title={title}>
     </Page>
   );
 };
