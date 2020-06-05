62c85617e2e0ff89bcba081e4b12393900c3c844
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-05 14:51:57 +0200

    Refactored old code

--- image-resizer-bot/bot.py
+++ image-resizer-bot/bot.py
@@ -1,7 +1,10 @@
 from pymysql import connect
 import cv2
 import requests
+import os
 
+os.mkdir("./temp", 0o0755)
+os.mkdir("./output", 0o0755)
 database = connect(host="localhost", user="root", passwd="")
 cursor = database.cursor()
 cursor.execute("SELECT COUNT(*) FROM zsbrybnik.posts")
@@ -10,15 +13,19 @@ cursor.execute("SELECT img FROM zsbrybnik.posts")
 postsImagesResultTuple = cursor.fetchall()
 postsImages = []
 
+dim = (250, 250)
+
 
 def workWithImage(postId: int):
     try:
         res = requests.get(postsImages[postId])
         image = res.content
-        with open("tempImg"+str(postId)+".jpg", "wb") as file:
-            print(postId)
+        with open("./temp/tempImg"+str(postId)+".jpg", "wb") as file:
             file.write(image)
             file.close()
+        cv2Image = cv2.imread("./temp/tempImg"+str(postId)+".jpg", 1)
+        data = cv2.resize(cv2Image, dim)
+        cv2.imwrite("./output/tempImg"+str(postId)+".jpg", data)
     except Exception as _err:
         print(_err)
 
@@ -27,8 +34,6 @@ for i in range(numberOfPosts):
     postsImages.append(postsImagesResultTuple[i][0])
 
 for i in range(numberOfPosts):
-    if i > 3:
-        break
     workWithImage(i)
 
 
