658c4e2bd2b1e0094bc743cd402fcc27a5a5dcbb
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-13 14:42:38 +0200

    Linted file

--- website/src/components/CodeBlock.tsx
+++ website/src/components/CodeBlock.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from 'react';
-import { Prism } from 'react-syntax-highlighter';
+import React, { FC } from "react";
+import { Prism } from "react-syntax-highlighter";
 
 interface CodeBlockProps {
   value: string;
   language: string;
 }
 
-const CodeBlock: FC<CodeBlockProps> = ({ value, language }: CodeBlockProps): JSX.Element => {
+const CodeBlock: FC<CodeBlockProps> = (
+  { value, language }: CodeBlockProps,
+): JSX.Element => {
   return (
     <Prism language={language}>
       {value}
