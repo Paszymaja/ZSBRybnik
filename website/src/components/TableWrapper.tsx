1e93064b6f4569d05fa3f042e197b634f12a17c1
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-14 17:11:24 +0200

    Deleted inconsistent design

--- website/src/components/TableWrapper.tsx
+++ website/src/components/TableWrapper.tsx
@@ -8,7 +8,6 @@ const TableWrapper = styled.table<TableWrapperProps>`
   border: 1px solid ${({ isDarkTheme }) => isDarkTheme ? "#fff" : "#ddd"};
   border-collapse: collapse;
   width: 100%;
-  margin-bottom: 15px;
   td,
   th {
     border: 1px solid ${({ isDarkTheme }) => isDarkTheme ? "#fff" : "#ddd"};
