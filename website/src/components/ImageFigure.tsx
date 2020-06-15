2d8f79e35b2549c6fcf6a23ee2a395dabf5f8178
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-15 15:42:23 +0200

    Changed style

--- website/src/components/ImageFigure.tsx
+++ website/src/components/ImageFigure.tsx
@@ -1,13 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ImageFigure = styled.figure`
   width: 100%;
   text-align: center;
   margin-left: auto;
   margin-right: auto;
-  @media all and (min-width: 768px) {
-    width: 80%;
-  }
 `;
 
 export default ImageFigure;
