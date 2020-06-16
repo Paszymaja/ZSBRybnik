e8ef4e4218f8934b6163ee2a7bb7d29ed87a415f
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-16 20:03:15 +0200

    Typed code

--- website/src/components/GalleryLogo.tsx
+++ website/src/components/GalleryLogo.tsx
@@ -1,11 +1,13 @@
-import styled from "styled-components";
+import styled, { StyledComponent } from "styled-components";
 
 interface GalleryLogoProps {
   isDarkTheme: boolean;
 }
 
-const GalleryLogo = styled.img<GalleryLogoProps>`
-  background: ${({ isDarkTheme }) => isDarkTheme ? "#fff" : "inherit"};
+const GalleryLogo: StyledComponent<"img", any, GalleryLogoProps, never> = styled
+  .img<GalleryLogoProps>`
+  background: ${({ isDarkTheme }: GalleryLogoProps) =>
+  isDarkTheme ? "#fff" : "inherit"};
   border-radius: 25px;
   max-width: fit-content;
   width: 40%;
