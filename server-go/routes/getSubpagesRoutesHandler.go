a559e28b93ee81ebee43e6e50eed4a86bf57a5c3
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-19 16:51:17 +0200

    Adjusted getSubpagesRoutes to not polish versions

--- server-go/routes/getSubpagesRoutesHandler.go
+++ server-go/routes/getSubpagesRoutesHandler.go
@@ -16,14 +16,14 @@ type subpagesRoutesJSON struct {
 
 // GetSubpagesRoutesHandler - handling get-subpages-routes route
 func GetSubpagesRoutesHandler(context *gin.Context) {
-	language := context.DefaultQuery("language", "pl")
+	language := context.Query("language")
 	database, ok := context.MustGet("database").(*sql.DB)
 	if !ok {
 		log.Fatalln("Can't find database in gin-gonic context")
 		context.AbortWithError(500, errors.New("Internal Server Error"))
 	} else {
-		query := "SELECT route, title FROM subpages WHERE language = ?"
-		result, err := database.Query(query, language)
+		query := "SELECT route, title FROM subpages WHERE language = \"pl\""
+		result, err := database.Query(query)
 		utils.ErrorHandler(err, false)
 		defer result.Close()
 		var subpagesRoutesArray []subpagesRoutesJSON
@@ -33,6 +33,23 @@ func GetSubpagesRoutesHandler(context *gin.Context) {
 			utils.ErrorHandler(err, false)
 			subpagesRoutesArray = append(subpagesRoutesArray, subpageRoute)
 		}
-		context.JSON(200, subpagesRoutesArray)
+		if language != "pl" {
+			query := "SELECT route, title FROM subpages WHERE language = ?"
+			result, err := database.Query(query, language)
+			utils.ErrorHandler(err, false)
+			defer result.Close()
+			for result.Next() {
+				var subpageRoute subpagesRoutesJSON
+				err := result.Scan(&subpageRoute.Route, &subpageRoute.Title)
+				utils.ErrorHandler(err, false)
+				for i, value := range subpagesRoutesArray {
+					if value.Route == subpageRoute.Route {
+						subpagesRoutesArray[i].Title = subpageRoute.Title
+					}
+				}
+			}
+		} else {
+			context.JSON(200, subpagesRoutesArray)
+		}
 	}
 }
