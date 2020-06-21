aaafffbadc34432c88d227074eaa42eed5f36c93
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-21 16:44:02 +0200

    Added title to slideOutMenuButton

--- website/src/components/SlideOutMenuButton.tsx
+++ website/src/components/SlideOutMenuButton.tsx
@@ -1,16 +1,24 @@
-import React, { useContext } from 'react';
-import SlideOutMenuButtonWrapper from './SlideOutMenuButtonWrapper';
-import GlobalContext from '../stores/globalStore';
-import { mdiBackburger, mdiMenu } from '@mdi/js';
-import { iconSize } from '../other/variables';
-import Icon from '@mdi/react';
+import React, { useContext } from "react";
+import SlideOutMenuButtonWrapper from "./SlideOutMenuButtonWrapper";
+import GlobalContext from "../stores/globalStore";
+import { mdiBackburger, mdiMenu } from "@mdi/js";
+import { iconSize } from "../other/variables";
+import Icon from "@mdi/react";
 
 const SlideOutMenuButton = () => {
   const { isSlideOutMenuOpenDispatcher } = useContext(GlobalContext);
-  const [isSlideOutMenuOpen, setIsSlideOutMenuOpen] = isSlideOutMenuOpenDispatcher;
+  const [isSlideOutMenuOpen, setIsSlideOutMenuOpen] =
+    isSlideOutMenuOpenDispatcher;
   return (
-    <SlideOutMenuButtonWrapper onClick={() => setIsSlideOutMenuOpen(!isSlideOutMenuOpen)}>
-      {isSlideOutMenuOpen ? <Icon path={mdiBackburger} size={iconSize} color="#fff" /> : <Icon path={mdiMenu} size={iconSize} color="#fff" />}
+    <SlideOutMenuButtonWrapper
+      title={isSlideOutMenuOpen
+        ? "Zamknij menu rozsuwane"
+        : "Otwórz menu rozuswane"}
+      onClick={() => setIsSlideOutMenuOpen(!isSlideOutMenuOpen)}
+    >
+      {isSlideOutMenuOpen
+        ? <Icon path={mdiBackburger} size={iconSize} color="#fff" />
+        : <Icon path={mdiMenu} size={iconSize} color="#fff" />}
     </SlideOutMenuButtonWrapper>
   );
 };
