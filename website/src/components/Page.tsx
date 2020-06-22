a16e194c1f67992aed12afbd696e1c31ebd032bc
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-22 18:36:48 +0200

    Added dynamic og title tag and fixed info about authors

--- website/src/components/Page.tsx
+++ website/src/components/Page.tsx
@@ -1,6 +1,11 @@
 import React, { FC, useEffect, ReactNode, useContext } from "react";
-import { Helmet } from 'react-helmet-async';
-import GlobalContext from '../stores/globalStore';
+import { Helmet } from "react-helmet-async";
+import GlobalContext, {
+  GlobalContextCompleteValues,
+  TitleDispatcher,
+  IsMobileDispatcher,
+  IsDarkThemeDispatcher,
+} from "../stores/globalStore";
 import ContentWrapper from "./ContentWrapper";
 import Presentation from "./Presentation";
 
@@ -10,17 +15,22 @@ interface PageProps {
 }
 
 const Page: FC<PageProps> = ({ title, children }): JSX.Element => {
-  const { titleDispatcher, isMobileDispatcher, isDarkThemeDispatcher } = useContext(GlobalContext);
-  const [titleLocal, setTitleLocal] = titleDispatcher;
-  const [isMobile] = isMobileDispatcher;
-  const [isDarkTheme] = isDarkThemeDispatcher;
+  const { titleDispatcher, isMobileDispatcher, isDarkThemeDispatcher }:
+    GlobalContextCompleteValues = useContext(GlobalContext);
+  const [titleLocal, setTitleLocal]: TitleDispatcher = titleDispatcher;
+  const [isMobile]: IsMobileDispatcher = isMobileDispatcher;
+  const [isDarkTheme]: IsDarkThemeDispatcher = isDarkThemeDispatcher;
+  const fixedTitle: string = `ZSB Rybnik ${
+    titleLocal !== "" ? "-" : ""
+  } ${titleLocal}`;
   useEffect((): void => {
     setTitleLocal(title);
   }, [title, setTitleLocal]);
   return (
     <>
       <Helmet>
-        <title>ZSB Rybnik {titleLocal !== "" ? "-" : ""} {titleLocal}</title>
+        <title>{fixedTitle}</title>
+        <meta name="og:title" content={fixedTitle} />
       </Helmet>
       <ContentWrapper isDarkTheme={isDarkTheme}>
         {children}
