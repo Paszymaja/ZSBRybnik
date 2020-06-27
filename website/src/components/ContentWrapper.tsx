e12f0656143cb803be58f34a1360f98f066626f3
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-27 21:00:36 +0200

    Typed code

--- website/src/components/ContentWrapper.tsx
+++ website/src/components/ContentWrapper.tsx
@@ -4,12 +4,18 @@ interface ContentWrapperProps {
   isDarkTheme: boolean;
 }
 
-const ContentWrapper: StyledComponent<"div", any, ContentWrapperProps, never> =
-  styled.div<ContentWrapperProps>`
+type ContentWrapperType = StyledComponent<
+  "div",
+  any,
+  ContentWrapperProps,
+  never
+>;
+
+const ContentWrapper: ContentWrapperType = styled.div<ContentWrapperProps>`
   margin: 0;
   height: fit-content;
   background: ${({ isDarkTheme }: ContentWrapperProps): string =>
-    isDarkTheme ? "#222" : "#eee"};
+  isDarkTheme ? "#222" : "#eee"};
   padding: 15px;
   width: 100%;
   h1, h2, h3, h4, h5, h6 {
