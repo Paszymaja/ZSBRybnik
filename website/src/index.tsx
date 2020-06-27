37f15d1f2587245b05aae6e2924cc6afbe8a5acb
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-06-27 18:27:00 +0200

    Formated files

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,12 +1,12 @@
-import React, { StrictMode } from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
+import App from "./App";
 import initTranslations from "./other/i18next";
-import * as serviceWorker from './serviceWorker';
-import ReactGA from 'react-ga';
-import consoleGreeting from './other/consoleGreeting';
+import * as serviceWorker from "./serviceWorker";
+import ReactGA from "react-ga";
+import consoleGreeting from "./other/consoleGreeting";
 
-ReactGA.initialize('UA-107728956-9');
+ReactGA.initialize("UA-107728956-9");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 export const rootElement: HTMLElement | null = document.getElementById("root");
@@ -18,7 +18,7 @@ render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 serviceWorker.register();
