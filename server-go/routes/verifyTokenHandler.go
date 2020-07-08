48e33753c91cf5b50984495d2dcfeb06d94e92ff
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-07-08 14:26:51 +0200

    Fixed token verification

--- server-go/routes/verifyTokenHandler.go
+++ server-go/routes/verifyTokenHandler.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"errors"
+
 	"github.com/gin-gonic/gin"
 	"zsbrybnik.pl/server-go/utils"
 )
@@ -14,5 +16,11 @@ func VerifyTokenHandler(context *gin.Context) {
 	var verifyTokenData verifyTokenJSON
 	err := context.Bind(&verifyTokenData)
 	utils.ErrorHandler(err, false)
-	context.JSON(200, verifyTokenData)
+	err = utils.VerifyToken(verifyTokenData.Token)
+	utils.ErrorHandler(err, false)
+	if err != nil {
+		context.AbortWithError(403, errors.New("Forbidden"))
+	} else {
+		context.JSON(200, "Ok!")
+	}
 }
