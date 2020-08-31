63df455635f1339bf29c4a4e0f8c8a2b930cfa07
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-08-31 18:01:22 +0200

    Added image fallback support

--- website/src/components/Image/Image.tsx
+++ website/src/components/Image/Image.tsx
@@ -2,16 +2,22 @@ import React, { FC } from "react";
 import ImageFigure from "./ImageFigure";
 import ImageWrapper from "./ImageWrapper";
 import ImageFigcaption from "./ImageFigcaption";
+import { useImage } from "react-image";
 
 interface ImageProps {
-  src: string;
+  src: string | string[];
   alt: string;
   text?: string;
 }
 
-const Image: FC<ImageProps> = (
-  { src, alt, text }: ImageProps,
-): JSX.Element => {
+const Image: FC<ImageProps> = ({
+  src: srcToFix,
+  alt,
+  text,
+}: ImageProps): JSX.Element => {
+  const { src } = useImage({
+    srcList: srcToFix,
+  });
   return (
     <ImageFigure>
       <ImageWrapper src={src} alt={alt} />
