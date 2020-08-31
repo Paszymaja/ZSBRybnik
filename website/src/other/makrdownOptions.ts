83ad0492a50563b4ebb5c7f203c8d6a8562f9cec
Krzysztof Zawisła <snowdropcurvemaster@gmail.com>
2020-08-31 18:08:42 +0200

    Fixed types

--- website/src/other/makrdownOptions.ts
+++ website/src/other/makrdownOptions.ts
@@ -46,12 +46,12 @@ const markdownOptions: MarkdownOptions = {
   namedCodesToUnicode: {
     plus: "\u002b",
     minus: "\u2212",
-    currentyear: new Date().getFullYear(),
-    currentday: new Date().getDay(),
-    currentmonth: new Date().getMonth(),
-    currenthour: new Date().getHours(),
-    currentminute: new Date().getMinutes(),
-    currentsecond: new Date().getSeconds(),
+    currentyear: new Date().getFullYear().toString(),
+    currentday: new Date().getDay().toString(),
+    currentmonth: new Date().getMonth().toString(),
+    currenthour: new Date().getHours().toString(),
+    currentminute: new Date().getMinutes().toString(),
+    currentsecond: new Date().getSeconds().toString(),
     currentdate: new Date().toString(),
   },
 } as MarkdownOptions;
